db73a71dfa5f3026cb859cec62d9a56ead7edf12
Claudia1233 <c87093899@gmail.com>
2025-11-05 22:05:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function verificarValor() {
+  const valor = 100; // puedes cambiar este valor para probar
+  const resultado = document.getElementById("resultado");
+
+  if (valor === 100) {
+    resultado.innerText = "🎉 ¡Felicidades! El valor es exactamente 100.";
+  }
+}
